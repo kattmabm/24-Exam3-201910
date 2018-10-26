933321ec8b9bb82248d1f7ce5fd326b220a23821
kattmabm <kattmabm@rose-hulman.edu>
2018-10-25 21:24:09 -0400

    Problem 3 Done

--- src/problem3.py
+++ src/problem3.py
@@ -2,8 +2,8 @@
 Exam 3, problem 3.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October, 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Braden Kattman.  October, 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import testing_helper
 
@@ -223,9 +223,15 @@ def problem3(seq_of_seq):
       :rtype: (list of int) | int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    for j in range(len(seq_of_seq)):
+        for k in range(len(seq_of_seq[j])):
+            if is_prime(seq_of_seq[j][k]):
+                return seq_of_seq[j]
+    return -1
 
 
 ###############################################################################
