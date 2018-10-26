9ec9576b150c8fff94eb2ba7391a3f473fbc5ead
kattmabm <kattmabm@rose-hulman.edu>
2018-10-25 21:22:17 -0400

    Problem 2 Done

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Exam 3, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Braden Kattman.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -90,9 +90,18 @@ def problem2(sequence):
       :type sequence [list]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    index = 0
+    top = 0
+    for k in range(len(sequence)):
+        if abs(sequence[k]) > top:
+            top = abs(sequence[k])
+            index = k
+    return index
+
 
 
 ###############################################################################
